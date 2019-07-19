1001efd64c4ab28caf9dfab9a2105a72f2ab0421
phlummox <phlummox@localhost>
2019-07-19 12:15:10 +0000

    add tests

---
+++ docker_test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source util_funcs.sh
+
+final_stage=$(tail -n 1 stages.txt)
+
+col_msg "$0: call toolchain"
+
+set -x
+echo 'int main(){return 42;}' > c_test.c
+docker run --rm  -v ${PWD}:/work -w /work phlummox/aarch64-cross-compiler:$final_stage gcc -static -o c_test c_test.c
+./c_test
+result=$?
+set +x
+
+echo $result
+if ((result != 42)) ; then
+  echo "failed!"
+  exit 1
+fi
+
